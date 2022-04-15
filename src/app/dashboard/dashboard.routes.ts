cb258ce886ecff36922b6cba9f2d52cede9ad173
rmorales91 <rey911207@gmail.com>
2022-04-15 18:33:31 -0400

    Initial Commit

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+import { Routes } from '@angular/router';
+
+
+export const dashboardRoutes: Routes = [
+  {path: '', component: EstadisticaComponent},
+  {path: 'ingreso-egreso', component: IngresoEgresoComponent},
+  {path: 'detalle', component: DetalleComponent}
+];
